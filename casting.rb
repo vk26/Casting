9ddc2fab1c8e46540c4160cd13faa54e735b2648
vk26 <vkosmos26@gmail.com>
2014-07-10 11:17:20 +0900

    Update casting.rb

--- casting.rb
+++ casting.rb
@@ -1,7 +1,8 @@
 module Casting
 	class Person
-		attr_accessor :name, :gender, :age
+		attr_reader :name, :gender, :age
 		def initialize(params={} )
+			raise 'Gender must be man or woman' unless ['man','woman'].include?(params[:gender])
 			@name = params[:name]
 			@gender = params[:gender]
 			@age = params[:age]
@@ -9,12 +10,14 @@ module Casting
 	end
 		
 	class Role < Person
-		attr_accessor :age_limit
+		def initialize(**params)
+			raise 'age for the role must be specified in the form (30..35) for example' unless params[:age].class == Range
+			super
+		end	
 
 	end	
 
 	class Candidate < Person
-		attr_accessor :age
 		attr_reader :appearance_array
 
 		def initialize(*arg)
@@ -23,8 +26,9 @@ module Casting
 		end	
 
 		def acts(params={})
-			appearance = Appearance.new(self, params[:role], params[:topic], params[:text], params[:duration])
-			@appearance_array << appearance
+			raise 'Candidate may only 1 attempt on the role' if self.appearance_array.map {|el| el.role}.include?(params[:role])
+			appearance = Appearance.new(self, params[:role], params[:topic], params[:text], params[:duration])			
+			@appearance_array << appearance 
 			appearance
 		end	
 
@@ -39,11 +43,12 @@ module Casting
 	end
 
 	class Appearance	
-		attr_accessor :candidate, :role, :topic, :text, :duration
-		attr_accessor :score_array
+		attr_accessor :candidate, :role, :topic, :text, :duration, :score_array
 		attr_reader :score_avr
 
 		def initialize(candidate, role, topic, text, duration)
+			raise 'The age of the candidate must is under the age limit of the role' unless role.age.to_a.include?(candidate.age)
+			raise 'The gender of the actor and the role must be the same' unless role.gender.eql?(candidate.gender)
 			@candidate = candidate
 			@role = role
 			@topic = topic
@@ -67,11 +72,8 @@ module Casting
 			raise 'The score should be not more than 10' if score > 10
 			raise 'Women will not put more than 7 points for the speech, which contains less than 30 words' if score > 7 && self.gender == 'woman' && appearance.text.size < 30
 			raise 'Men will not put less than 7 points girls 18-25 years' if score < 7 && self.gender == 'man' && appearance.candidate.gender == 'woman' && appearance.candidate.age >= 18 && appearance.candidate.age <= 25
-			
-			appearance.score_array = score
-			
+			appearance.score_array = score			
 		end
-
 	end
 
 end	
